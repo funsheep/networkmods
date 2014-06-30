2625f7594a00379f1a33c784107eaaac9049237b
Hendrik Renken <h.renken@lodige.com>
2014-06-30 07:10:48 +0200

    Updated to redesign of LoomiServlet.

--- loomi/src/com/lodige/platform/loomi/Servlet.java
+++ loomi/src/com/lodige/platform/loomi/Servlet.java
@@ -7,24 +7,29 @@ package com.lodige.platform.loomi;
 import github.javaappplatform.commons.events.IListener;
 import github.javaappplatform.platform.extension.ExtensionRegistry;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 import com.lodige.loomi.ILoomiAPI;
-import com.lodige.loomi.engine.LoomiServlet;
+import com.lodige.loomi.engine.ALoomiServlet;
+import com.vaadin.server.UIProvider;
 
 /**
  * @author renken
  *
  */
 @WebServlet(value = "/*", asyncSupported = true)
-public class Servlet extends LoomiServlet
+public class Servlet extends ALoomiServlet
 {
 
 	public static final String EXTPOINT_UI_LIFECYCLE = "com.lodige.platform.loomi.ui.lifecycle";
+	public static final String EXTPOINT_UI_PROVIDER = "com.lodige.platform.loomi.ui.provider";
 
+	private static UIProvider[] PROVIDER = null;
+	
 
 	/**
 	 * {@inheritDoc}
@@ -39,6 +44,20 @@ public class Servlet extends LoomiServlet
 			this.addListener(ILoomiAPI.E_UISESSION_STARTING, li);
 			this.addListener(ILoomiAPI.E_UISESSION_CLOSING, li);
 		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected synchronized UIProvider[] getUIProvider()
+	{
+		if (PROVIDER == null)
+		{
+			PROVIDER = ExtensionRegistry.getServices(EXTPOINT_UI_PROVIDER).stream().toArray(UIProvider[]::new);
+			System.out.println("Found UIProvider: " + Arrays.toString(PROVIDER));
+		}
+		return PROVIDER;
 	}
 	
 	private static final long serialVersionUID = 1L;
