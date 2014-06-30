ce90cfa67b99c2b08e34adc9a0add39bb854764d
Hendrik Renken <h.renken@lodige.com>
2014-06-30 16:55:32 +0200

    Responsive database connection configuration. Broken connections are now much better detected.

--- dbaccess/src/com/lodige/dbaccess/BootEntry.java
+++ dbaccess/src/com/lodige/dbaccess/BootEntry.java
@@ -10,6 +10,7 @@ import github.javaappplatform.platform.PlatformException;
 import github.javaappplatform.platform.boot.IBootEntry;
 import github.javaappplatform.platform.extension.Extension;
 import github.javaappplatform.platform.extension.ExtensionRegistry;
+import github.javaappplatform.platform.extension.ServiceInstantiationException;
 
 import java.sql.SQLException;
 
@@ -48,17 +49,42 @@ public class BootEntry implements IBootEntry
 		String dbuser = Platform.getOptionValue(O_DBUSER, null);
 		String dbpswd = Platform.getOptionValue(O_DBPSWD, null);
 
-		IjdbcURIGenerator generator = ExtensionRegistry.getService(IjdbcURIGenerator.EXTPOINT_JDBCURIGEN);
-		if (generator == null)
-			throw new PlatformException("Could not instantiate IjdbcURIGenerator.");
+		Extension genex = ExtensionRegistry.getExtension(IjdbcURIGenerator.EXTPOINT_JDBCURIGEN);
+		if (genex == null)
+			throw new PlatformException("Could not find IjdbcURIGenerator extension.");
+
+		IjdbcURIGenerator generator;
+		try
+		{
+			generator = genex.getService();
+		}
+		catch (ServiceInstantiationException e1)
+		{
+			throw new PlatformException("Could not instantiate JDBC driver.", e1);
+		}
 		
 		BasicDataSource pool = new BasicDataSource();
 		pool.setUsername(dbuser);
 		pool.setPassword(dbpswd);
 		pool.setUrl(generator.generateURI(dbhost, dbport, dbname));
+		this.initPool(pool, e, genex);
 		ExtensionRegistry.registerSingleton(DataSource.class.getName(), EXTPOINT_DBCP, pool, null);
 		
 		LOGGER.info("Started connection pool with URI {}", pool.getUrl());
+	}
+	
+	private void initPool(BasicDataSource pool, Extension dbaccess, Extension generator)
+	{
+		String validQuery = generator.getProperty("validationquery");
+		if (validQuery != null)
+			pool.setValidationQuery(validQuery);
+		pool.setTestOnBorrow(dbaccess.getProperty("testOnBorrow", true));
+		pool.setTestWhileIdle(dbaccess.getProperty("validationInterval", -1) != -1);
+		pool.setTimeBetweenEvictionRunsMillis(dbaccess.getProperty("timeBetweenEvictionRunsMillis", -1));
+		pool.setMinEvictableIdleTimeMillis(dbaccess.getProperty("minEvictableIdleTimeMillis", -1));
+		pool.setRemoveAbandoned(dbaccess.getProperty("removeAbandoned", true));
+		pool.setRemoveAbandonedTimeout(dbaccess.getProperty("removeAbandonedTimeout", 300));
+		pool.setValidationQueryTimeout(dbaccess.getProperty("validationquerytimeout", 10));
 	}
 
 	/**
